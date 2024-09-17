cef54b80eb828d6209acf2a4edec4b8f57d9a62e
Felix Rödén <felix.roden@outlook.com>
2024-09-17 11:53:12 +0200

    Initial commit

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Calculator {
+    public Calculator() {
+
+    }
+
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+}
---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    Calculator calculator;
+    int num1;
+    int num2;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+        num1 = 10;
+        num2 = 5;
+    }
+
+    @org.junit.jupiter.api.Test
+    void add() {
+        // Förväntat resultat
+        int expected = 15;
+
+        // Anropa add()-metoden
+        int result = calculator.add(num1, num2);
+
+        // Kontrollera att resultatet är korrekt
+        assertEquals(expected, result, "add() metoden bör returnera summan av två tal");
+    }
+}
